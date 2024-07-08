667b7b2ba951926a814fdd90ca31fb699bd40bcb
CGladwin <calebgladwin@gmail.com>
2024-07-08 16:00:47 -0400

    tested optimization for ppm output (negligible)

--- src/camera.py
+++ src/camera.py
@@ -10,7 +10,7 @@ def ray_colour(r: ray) -> color:
 
 if __name__ == "__main__":
     
-    aspect_ratio = 16.0 / 9.0
+    aspect_ratio = 16.0 / 9.0 #ideal ratio
     image_width = 400
 
     # Calculate the image height, and ensure that it's at least 1.
@@ -18,14 +18,15 @@ if __name__ == "__main__":
     if image_height < 1: 
         image_height = 1
     viewport_height = 2.0
-    viewport_width = viewport_height * (float(image_width)/image_height)
+    integer_ratio = (float(image_width)/image_height)
+    viewport_width = viewport_height * integer_ratio
 
     #Camera
 
     focal_length = 1.0
     viewport_height = 2.0
-    viewport_width = viewport_height * (float(image_width)/image_height)
-    camera_center = point3([0, 0, 0])
+    viewport_width = viewport_height * integer_ratio
+    camera_center = point3([0, 0, 0]) #camera center at the origin
 
     # Calculate the vectors across the horizontal and down the vertical viewport edges.
     viewport_u = vec3([viewport_width, 0, 0])
@@ -37,7 +38,9 @@ if __name__ == "__main__":
 
     # Calculate the location of the upper left pixel.
     viewport_upper_left = camera_center - vec3([0, 0, focal_length]) - viewport_u/2 - viewport_v/2
-    pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v)
+    # starts with camera center, moves in z axis to intersect with viewport, moves left and up
+    pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v)/2
+
 
     # Render
     image_width : int =  256
@@ -50,6 +53,7 @@ if __name__ == "__main__":
         for j in range(image_height):
             progress_indicator("scanlines remaining: ",image_height,image_height-j)
             for i in range(image_width):
+                # find each pixel by offsetting 0,0 based on pixel deltas
                 pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v)
                 ray_direction = pixel_center - camera_center
                 r = ray(camera_center,ray_direction)
@@ -58,20 +62,3 @@ if __name__ == "__main__":
     print("\nDone.") #newline necessary because output stream after progress_indicator call is pointing to end of previous line, not newline
     view_ppm_img(output_ppm_path)
             
-
-#     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
-#     for (int j = 0; j < image_height; j++) {
-#         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-#         for (int i = 0; i < image_width; i++) {
-#             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-#             auto ray_direction = pixel_center - camera_center;
-#             ray r(camera_center, ray_direction);
-
-#             color pixel_color = ray_color(r);
-#             write_color(std::cout, pixel_color);
-#         }
-#     }
-
-#     std::clog << "\rDone.                 \n";
-# }
