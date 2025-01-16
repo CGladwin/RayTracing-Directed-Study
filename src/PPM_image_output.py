151cad440fe11d383708cfe4eec17e53236b9614
CGladwin <calebgladwin@gmail.com>
2025-01-16 02:19:29 -0500

    removed python progress indicator (will need to redo in C++)

--- src/PPM_image_output.py
+++ src/PPM_image_output.py
@@ -14,21 +14,6 @@ import timeit
 - check out dithering 
 """
 
-def progress_indicator(base_text: str,start_value: int,current_value: int) -> None:
-    """overrides previous string with current value + spaces based on length of initial value"""
-    # used to indicate progress of loop
-    def get_digits(number):
-        '''helper function that outperforms len(str(number))'''
-        # https://stackoverflow.com/questions/2189800/how-to-find-length-of-digits-in-an-integer
-        if number > 0:
-            return int(math.log10(number))+1
-        elif number == 0:
-            return 1
-        else:
-            raise ValueError("progress indicator should not reach negative numbers")
-    spaces_necessary = get_digits(start_value)-get_digits(current_value)
-    print("\r%s%d" % (base_text,current_value)+" "*spaces_necessary,end="",flush=True)
-
 def view_ppm_img(img_path: str, img_title:str):
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
