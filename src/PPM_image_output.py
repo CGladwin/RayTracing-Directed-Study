404c9938e172a2cf36ae4e2f044465bfc35a332d
CGladwin <calebgladwin@gmail.com>
2025-01-16 00:08:03 -0500

    removed python progress, updated viewer to be system agnostic, changed batch file to shell script

--- src/PPM_image_output.py
+++ src/PPM_image_output.py
@@ -35,7 +35,7 @@ def hello_world_ppm(output_ppm_dir: str = ".",
                     image_height: int = 256) -> str:
     """generates PPM file, sent to directory given in param, returns path"""
     appendlist = []
-    output_ppm_path = os.path.abspath(output_ppm_dir)+'\\rgb_image.ppm'
+    output_ppm_path = os.path.join(output_ppm_dir,'rgb_image.ppm')
     with open(output_ppm_path,"w+") as f:
         f.write("P3\n%d %d\n255\n" % (image_width,image_height))
         for j in range(image_height):
@@ -48,36 +48,6 @@ def hello_world_ppm(output_ppm_dir: str = ".",
         print("\nDone.") #newline necessary because output stream after progress_indicator call is pointing to end of previous line, not newline
     return output_ppm_path
 
-def hello_world_ppm_optimized(output_ppm_dir: str = ".",
-                    image_width: int = 256,
-                    image_height: int = 256) -> str:
-    """generates PPM file, sent to directory given in param, returns path"""
-    # 30% faster based on timeit testing with IO stripped (likely because of more usage of numpy)
-    # too harded coded to use right now, but here's a good source to look into this stuff more:
-    # https://news.ycombinator.com/item?id=15522428
-    # ideas: array broadcasting + 
-
-    output_ppm_path = os.path.abspath(output_ppm_dir)+'\\optimized_image.ppm'
-    X = np.array([i for i in range(image_width)]*image_height)
-    Y = np.array([i for i in range(image_height) for j in range(image_width)])
-    # print(pixel_array)
-    with open(output_ppm_path,"w+") as f:
-        f.write("P3\n%d %d\n255\n" % (image_width,image_height))
-        rgb_r_func = lambda i: i/(image_width-1)
-        rgb_g_func = lambda j: j/(image_height-1)
-        rgb_r = rgb_r_func(X)
-        rgb_g = rgb_g_func(Y)
-        rgb_b = np.zeros((256*256))
-        rgb_list = np.vstack((rgb_r,rgb_g,rgb_b)).T
-
-        for rgb_index in range(len(rgb_list)):
-            # progress_indicator("scanlines remaining: ",image_height,image_height+1-rgb_index)
-            pixel_colour = color(rgb_list[rgb_index])
-            f.write(pixel_colour.write_colour())
-
-    print("\nDone.") #newline necessary because output stream after progress_indicator call is pointing to end of previous line, not newline
-    return output_ppm_path
-
 def view_ppm_img(img_path: str, img_title:str):
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
@@ -86,7 +56,7 @@ def view_ppm_img(img_path: str, img_title:str):
     plt.show()
 
 if __name__ == "__main__":
-    ppm_path = hello_world_ppm("images")
+    ppm_path = hello_world_ppm("src/images")
     view_ppm_img(ppm_path,"first image")
     # print(timeit.timeit('hello_world_ppm_optimized("images")','from __main__ import hello_world_ppm_optimized',number=100))
     # print(timeit.timeit('hello_world_ppm("images")','from __main__ import hello_world_ppm',number=100))
