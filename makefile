9b450b2c717de2f807d65e56ddc25f8bd7d2fe90
CGladwin <calebgladwin@gmail.com>
2025-02-02 19:27:27 -0500

    added interval class
    class to manage real-valued intervals with a minimum and a maximum

--- makefile
+++ makefile
@@ -8,6 +8,8 @@ OUTPUT = $(SRC_DIR)/images/output.png
 # default target
 all: compile_cpp run_cpp
 
+total: compile_cpp run_cpp clean doxy
+
 # Compile the program with optimizations
 compile_cpp:
 	@g++ $(CPP_FILE) -o $(EXECUTABLE)
@@ -19,6 +21,7 @@ run_cpp: compile_cpp
 # Compile the program with debugging symbols and no optimizations
 debug:
 	@g++ -g -O0 $(CPP_FILE) -o $(EXECUTABLE)
+	@echo "Debug build complete. Use a debugger (e.g., gdb) to run the program."
 
 # remove executable
 clean:
@@ -27,6 +30,10 @@ clean:
 # remove png
 png_clean:
 	@rm -f $(OUTPUT) 
+
+# recompile doxygen
+doxy:
+	@doxygen $(SRC_DIR)/doxyfile
 
 # Tells Make that these are not files
 .PHONY: all compile_cpp run_cpp clean png_clean
--- src/include/sphere.hpp
+++ src/include/sphere.hpp
@@ -19,7 +19,7 @@ class sphere : public hittable {
      * @return false 
      */
     //!doxygen test
-    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
@@ -33,9 +33,9 @@ class sphere : public hittable {
 
         // Find the nearest root that lies in the acceptable range.
         auto root = (h - sqrtd) / a;
-        if (root <= ray_tmin || ray_tmax <= root) {
+        if (!ray_t.surrounds(root)) {
             root = (h + sqrtd) / a;
-            if (root <= ray_tmin || ray_tmax <= root)
+            if (!ray_t.surrounds(root))
                 return false;
         }
 
